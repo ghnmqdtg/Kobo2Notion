4761ae11e526c4d87e61726aab6778431242c88d
ghnmqdtg <ghnmqdtg@gmail.com>
2024-08-21 21:00:09 +0800

    Feat: Fetch the sqlite file from kobo e-reader

--- export.sh
+++
@@ -1,5 +0,0 @@
-# Copy the KoboReader.sqlite file to the current directory
-cp /Volumes/KOBOeReader/.kobo/KoboReader.sqlite .
-
-# Run the script
-python kobo2notion.py
--- kobo2notion.py
+++ kobo2notion.py
@@ -12,6 +12,17 @@ if __name__ == "__main__":
     # Check if the environment variables are loaded
     assert os.environ["NOTION_API_KEY"] is not None, "NOTION_API_KEY is not set"
     assert os.environ["NOTION_DB_ID"] is not None, "NOTION_DB_ID is not set"
+    # Check if it's in dev mode
+    if os.environ["DEV_MODE"] == "true":
+        DEV_MODE = True
+    else:
+        DEV_MODE = False
+    # Copy the KoboReader.sqlite file to temp/KoboReader.sqlite
+    # This is to avoid conflicts with the original file
+    # Create a temp directory if it doesn't exist
+    os.makedirs("temp", exist_ok=True)
+    # Copy the KoboReader.sqlite file to temp/KoboReader.sqlite
+    os.system(f"cp {os.environ['SQLITE_SOURCE']} temp/KoboReader.sqlite")
 
     # Initialize the Notion client
     notion = Client(auth=os.environ["NOTION_API_KEY"])
