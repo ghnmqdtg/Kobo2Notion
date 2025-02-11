7fd92693be02724eeda099d3c75c9179b4061c5f
Tweizh <ghnmqdtg@gmail.com>
2025-02-11 16:40:55 +0800

    Feat: Reorder the initiation process

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,6 +4,10 @@ import { electronApp, optimizer, is } from '@electron-toolkit/utils';
 import icon from '../../resources/icon.png?asset';
 import fs from 'fs/promises';
 import path from 'path';
+import dotenv from 'dotenv';
+
+const envPath = path.resolve(__dirname, '../../.env');
+const exampleEnvPath = path.resolve(__dirname, '../../.env.example');
 
 async function updateEnvFile(entries: { key: string, value: string }[]): Promise<void> {
   const envPath = path.resolve(__dirname, '../../.env');
@@ -47,9 +51,6 @@ function cleanEnvKeys(): void {
 }
 
 async function ensureEnvFile(): Promise<void> {
-  const envPath = path.resolve(__dirname, '../../.env');
-  const exampleEnvPath = path.resolve(__dirname, '../../.env.example');
-
   try {
     await fs.access(envPath);
   } catch {
@@ -119,38 +120,46 @@ function createWindow(): void {
   });
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(async () => {
-  await ensureEnvFile();
-  // Set app user model id for windows
-  electronApp.setAppUserModelId('com.electron');
+function initializeApp(): void {
+  // This method will be called when Electron has finished
+  // initialization and is ready to create browser windows.
+  // Some APIs can only be used after this event occurs.
+  app.whenReady().then(async () => {
+    // Set app user model id for windows
+    electronApp.setAppUserModelId('com.electron');
 
-  // Default open or close DevTools by F12 in development
-  // and ignore CommandOrControl + R in production.
-  // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
-  app.on('browser-window-created', (_, window) => {
-    optimizer.watchWindowShortcuts(window);
+    // Default open or close DevTools by F12 in development
+    // and ignore CommandOrControl + R in production.
+    // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
+    app.on('browser-window-created', (_, window) => {
+      optimizer.watchWindowShortcuts(window);
+    });
+
+    createWindow();
+
+    app.on('activate', function () {
+      // On macOS it's common to re-create a window in the app when the
+      // dock icon is clicked and there are no other windows open.
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
   });
 
-  createWindow();
-
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  // Quit when all windows are closed, except on macOS. There, it's common
+  // for applications and their menu bar to stay active until the user quits
+  // explicitly with Cmd + Q.
+  app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
   });
-});
+}
 
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
+ensureEnvFile().then(() => {
+  cleanEnvKeys();
 
-// In this file you can include the rest of your app"s specific main process
-// code. You can also put them in separate files and require them here.
+  // Load environment variables from .env file
+  dotenv.config({ path: envPath });
+  console.log('cleaned env keys: ', process.env.NOTION_API, process.env.NOTION_DB, process.env.GEMINI_API, process.env.GEMINI_MODEL, process.env.SUMMARIZE_ENABLED, process.env.SUMMARIZE_LANGUAGE);
+
+  initializeApp();
+});
