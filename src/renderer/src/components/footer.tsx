75c28a9acc03602db1be542e2997274ffc3f3001
Tweizh <ghnmqdtg@gmail.com>
2025-02-12 17:10:16 +0800

    Feat: Disable the export feature when offline

--- src/renderer/src/components/footer.tsx
+++ src/renderer/src/components/footer.tsx
@@ -1,5 +1,7 @@
 import { Button } from '@/components/ui/button';
 import { Progress } from '@/components/ui/progress';
+import { useNetworkState } from '@uidotdev/usehooks';
+import { AlertCircle } from 'lucide-react';
 
 interface FooterProps {
     selectedCount: number;
@@ -18,11 +20,18 @@ export function Footer({
     currentStep,
     onExport
 }: FooterProps) {
+    const isOnline = useNetworkState();
+
     return (
-        <div id="footer" className="fixed bottom-0 bg-background backdrop-blur-none border-t">
+        <div className="w-4/5 fixed bottom-0 bg-background backdrop-blur-none border-t">
             <div className="container mx-auto px-4 h-16 flex items-center justify-between">
                 <div className="flex flex-col gap-2 flex-1 mr-4">
-                    {isExporting ? (
+                    {!isOnline.online ? (
+                        <div className="flex items-center space-x-2 text-destructive/95">
+                            <AlertCircle className="h-4 w-4" />
+                            <span className="text-md font-bold">Network is not available. Please check your network settings.</span>
+                        </div>
+                    ) : isExporting ? (
                         <>
                             <div className="flex justify-between text-sm text-muted-foreground">
                                 <span>{currentBook}</span>
@@ -39,8 +48,17 @@ export function Footer({
                         </div>
                     )}
                 </div>
-                <Button className="text-md font-bold" onClick={onExport} disabled={selectedCount === 0 || isExporting}>
-                    {isExporting ? 'Exporting...' : 'Export to Notion'}
+                <Button
+                    className={`text-md font-bold ${!isOnline.online ? 'bg-destructive/60' : ''}`}
+                    onClick={onExport}
+                    disabled={selectedCount === 0 || isExporting || !isOnline.online}
+                >
+                    {!isOnline.online
+                        ? 'No Network Connection ;('
+                        : isExporting
+                            ? 'Exporting...'
+                            : 'Export to Notion'
+                    }
                 </Button>
             </div>
         </div>
