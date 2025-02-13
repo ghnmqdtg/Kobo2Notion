df7bf6b57dc0222a71fdfbf4b9188ad3b78b4b8d
Tweizh <ghnmqdtg@gmail.com>
2025-02-13 17:10:22 +0800

    Fix: Fix the datatype of the output of `exportBook`

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -4,7 +4,7 @@ import { EnvironmentConfig } from '../config/env.config';
 
 export interface IElectronAPI {
   getBooks: () => Promise<Book[]>;
-  exportBook: (book: Book) => Promise<void>;
+  exportBook: (book: Book) => Promise<{ parentPageId: string, highlightPageId: string }>;
   summarizeBook: (book: Book, parentPageId: string) => Promise<void>;
   fetchBookCover: (imageId: string) => Promise<string>;
   updateEnvValue: (entries: { key: string, value: string }[]) => Promise<boolean>;
