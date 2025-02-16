5d2b217aea15b7e87d8b986787e6865708b88d18
Tweizh <ghnmqdtg@gmail.com>
2025-02-16 11:46:36 +0800

    Feat: Add font `jf-openhuninn-2.1`

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,5 +1,6 @@
 import './assets/main.css';
 import './assets/global.css';
+import './assets/index.css';
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
   content: ["./src/renderer/index.html", "./src/renderer/src/**/*.{svelte,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['"JF Open Huninn"', ...fontFamily.sans],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
