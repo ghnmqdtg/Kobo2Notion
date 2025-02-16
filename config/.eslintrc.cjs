552a271f16a5412ff43e5b8f7b244c1641e09c1a
Tweizh <ghnmqdtg@gmail.com>
2025-02-16 12:57:51 +0800

    Chore: Move config files into a single config folder

---
+++ config/.eslintrc.cjs
@@ -0,0 +1,9 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution')
+
+module.exports = {
+  extends: [
+    '@electron-toolkit/eslint-config-ts/recommended',
+    '@electron-toolkit/eslint-config-prettier'
+  ]
+} 
