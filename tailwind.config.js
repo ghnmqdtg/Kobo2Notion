2bed20eb4f6d1fb66b67a2c2f02b35270c8f9342
Tweizh <ghnmqdtg@gmail.com>
2026-03-14 15:44:06 +0800

    Style: Update design tokens and font configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,8 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['"JF Open Huninn"', '"Noto Sans TC"', ...fontFamily.sans],
+        sans: ['Geist', '"Noto Sans TC"', '"JF Open Huninn"', ...fontFamily.sans],
+        mono: ['"JetBrains Mono"', ...fontFamily.mono],
         serif: ['"Noto Serif TC"', ...fontFamily.serif]
       },
       colors: {
